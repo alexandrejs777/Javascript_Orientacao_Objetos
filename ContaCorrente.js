a14b6a9381746a49dfaf6318d690121d28c56929
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-01-23 23:35:30 -0300

    Modulos

---
+++ ContaCorrente.js
@@ -0,0 +1,18 @@
+export class ContaCorrente {
+    agencia;
+    /* #saldo = 0; Atributo privado aguardando a aceitação da convenção. https://github.com/tc39/proposal-class-fields#private-fields */
+    _saldo = 0; /* Essa é a convenção atual, mas o atributo não é privado na prática. 
+    O underline antes do atributo só indica que se trata de uma variável privada. */
+  
+    depositar(valor) {
+      if (valor > 0) return;
+      this._saldo += valor;
+    }
+  
+    sacar(valor) {
+      if (this._saldo >= valor) {
+        this._saldo -= valor;
+        return valor;
+      }
+    }
+  }
--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  agencia;
-  /* #saldo = 0; Atributo privado aguardando a aceitação da convenção. https://github.com/tc39/proposal-class-fields#private-fields */
-  _saldo = 0; /* Essa é a convenção atual, mas o atributo não é privado na prática. 
-  O underline antes do atributo só indica que se trata de uma variável privada. */
-
-  depositar(valor) {
-    if (valor > 0) return;
-    this._saldo += valor;
-  }
-
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Alexandre";
