7ef2fe8dee7b7b4b42ca78dd6b92d9253e349ba7
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-01-23 21:20:55 -0300

    Classes e Metodos

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 class Cliente {
-    nome;
-    cpf;
-    agencia;
-    saldo;
+  nome;
+  cpf;
+}
+
+class ContaCorrente {
+  agencia;
+  saldo;
+
+  sacar(valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Alexandre";
 cliente1.cpf = 11122233301;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Bruna";
 cliente2.cpf = 88822233301;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteAlexandre = new ContaCorrente();
+console.log("Valor do saldo inicial:");
+contaCorrenteAlexandre.saldo = 0;
+contaCorrenteAlexandre.agencia = 1001;
+
+console.log(contaCorrenteAlexandre.saldo);
+console.log("Valor do saque:");
+contaCorrenteAlexandre.saldo = 100;
+console.log(contaCorrenteAlexandre.saldo);
+contaCorrenteAlexandre.sacar(250);
+
+console.log("Saldo atualizado:");
+console.log(contaCorrenteAlexandre.saldo);
+console.log(cliente1);
+console.log(cliente2);
