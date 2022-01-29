484846e8b906b6ba475b7ea0ca119544afec7716
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-01-29 10:52:12 -0300

    Construtores

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,41 +1,47 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
-    agencia;
-    _cliente;
+  agencia;
+  _cliente;
+  /* #saldo = 0; Atributo privado aguardando a aceitação da convenção. https://github.com/tc39/proposal-class-fields#private-fields */
+  _saldo = 0; /* Essa é a convenção atual, mas o atributo não é privado na prática. 
+        O underline antes do atributo só indica que se trata de uma variável privada. */
 
-    set cliente(novoValor){
-      if (novoValor instanceof Cliente){
-        this._cliente = novoValor;
-      }
-    }
 
-    get cliente(){
-      return this._cliente;
-    }
-
-    /* #saldo = 0; Atributo privado aguardando a aceitação da convenção. https://github.com/tc39/proposal-class-fields#private-fields */
-    _saldo = 0; /* Essa é a convenção atual, mas o atributo não é privado na prática. 
-    O underline antes do atributo só indica que se trata de uma variável privada. */
   
-    get saldo(){
-      return this._saldo;
-    }
-
-    depositar(valor) {
-      if (valor <= 0) return;
-      this._saldo += valor;
-    }
-  
-    sacar(valor) {
-      if (this._saldo >= valor) {
-        this._saldo -= valor;
-        return valor;
-      }
-    }
-
-    transferir(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
   }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  constructor(cliente, agencia) {
+    this.cliente = cliente;
+    this.agencia = agencia;
+  }
+
+  depositar(valor) {
+    if (valor <= 0) return;
+    this._saldo += valor;
+  }
+
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
+    }
+  }
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
+}
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Alexandre";
-cliente1.cpf = 11122233301;
+const cliente1 = new Cliente("Alexandre", 11122233301);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Bruna";
-cliente2.cpf = 88822233301;
+const cliente2 = new Cliente("Bruna", 88822233301);
 
-const conta1 = new ContaCorrente();
-conta1.agencia = 1001;
-conta1.cliente = cliente1;
+const conta1 = new ContaCorrente(cliente1, 1001);
 conta1.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 120;
-
+const conta2 = new ContaCorrente(cliente2, 120);
 
 conta1.transferir(200, conta2);
 
-console.log(conta2.saldo);
-// console.log(conta1);
+// console.log(conta2.saldo);
+console.log(cliente1.cpf);
